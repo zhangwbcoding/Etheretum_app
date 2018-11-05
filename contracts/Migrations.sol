34e627fb19ca6f4adef609a0a546fbde1d68b1b9
snark <zhangwenbo@zju.edu.cn>
2018-11-05 19:34:41 +0800

    mod

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.4.23; 
 
 contract Migrations {
-    address public owner; 
-    uint public last_completed_migration; 
+	address public owner; 
+	uint public last_completed_migration; 
 
     constructor()public {
         owner = msg.sender; 
     }
 
   	modifier restricted() {
-    	if (msg.sender == owner)
+    	if (msg.sender == owner) _;
   	}
 
   	function setCompleted(uint completed)public restricted {
