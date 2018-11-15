f03bdc56888dbf8f311cef8222550669c4881259
snark <zhangwenbo@zju.edu.cn>
2018-11-15 22:28:57 +0800

    3

--- app/assets/js/chainhome/property-left.js
+++ app/assets/js/chainhome/property-left.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
     alert("ready~");
     var web3Provider = new Web3.providers.HttpProvider("http://192.168.33.88:8545");
 	var web3 = new Web3(web3Provider);
-	$.getJSON ("./build/contracts/Hello_mshk_top.json", function (data,status){  
+	$.getJSON ("../build/contracts/Hello_mshk_top.json", function (data,status){  
     	Hello_mshk_top =web3.eth.contract(data.abi).at("0x05c9f8a1ac1558123a01f5b157d1be3eeae6ca7f");
-   	    var re = Hello_mshk_top.say();
+           var re = Hello_mshk_top.say();
+           
     });
 });
